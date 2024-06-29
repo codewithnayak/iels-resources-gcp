f3aabe31a1c060ca4bc1d86007175deadbc7dd7c
codewithnayak <codewithnayak@gmail.com>
2024-06-29 21:26:00 +0100

    fix:removed cerd files

--- bucket/backend.tf
+++ bucket/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "gcs" {
     bucket     = "iels-terraform-state"
     prefix     = "terraform/state"  # Optional: Set a prefix for your state files
-    credentials = "path/to/your/creds.json"  # Path to your GCP service account credentials
+    
   }
 }
 
