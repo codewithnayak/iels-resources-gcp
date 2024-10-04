05da5ac6bae384d08441f8c2816a54982c84ab74
codewithnayak <codewithnayak@gmail.com>
2024-10-04 14:00:38 +0100

    feat: added iam helper bash file

---
+++ add-roles-sa-check.sh
@@ -0,0 +1,19 @@
+#¡/bin/bash
+## Change your project id , if needed
+project="primeval-nectar-431120-j5"
+sa="terraform-service-account@primeval-nectar-431120-j5.iam.gserviceaccount.com"
+role="roles/compute.instanceAdmin"
+set -e
+
+gcloud config set project $project
+
+gcloud projects add-iam-policy-binding $project \
+    --member="serviceAccount:terraform-service-account@primeval-nectar-431120-j5.iam.gserviceaccount.com" \
+    --role="roles/compute.instanceAdmin"
+
+##Check assigned roles
+echo "*** Check assigned roles $project $sa ***"
+gcloud projects get-iam-policy $project \
+    --flatten="bindings[].members" \
+    --format="table(bindings.role)" \
+    --filter="bindings.members:serviceAccount:terraform-service-account@primeval-nectar-431120-j5.iam.gserviceaccount.com"
