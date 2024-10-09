5d6e9d034176f0f4e7feedf7e79984e1ec0eab94
codewithnayak <codewithnayak@gmail.com>
2024-10-09 17:20:34 +0100

    fix: correct formt to get ip added

--- gce-advanced/output.tf
+++ gce-advanced/output.tf
@@ -1,3 +1,3 @@
 output "external_ips" {
-  value = [for instance in data.google_compute_instance.example_instances : instance.network_interface.access_config.nat_ip]
+  value = [for instance in data.google_compute_instance.example_instances : instance.network_interface[0].access_config[0].nat_ip]
 }
