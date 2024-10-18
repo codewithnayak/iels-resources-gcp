82aed5cd0b13d7cd91147eae7b3742eada3a879d
codewithnayak <codewithnayak@gmail.com>
2024-10-19 00:28:34 +0100

    feat: added k8 module

--- k8-sa/main.tf
+++ k8-sa/main.tf
@@ -1,10 +1,9 @@
-resource "kubernetes_service_account" "example" {
-  metadata {
-    name      = var.kubernetes_service_account_name
-    namespace = var.kubernetes_service_account_namespace
-
-    annotations = var.annotate_service_account ?  {
-      "iam.gke.io/gcp-service-account" = var.gcp_service_account_email
-    } : {}
-  }
+module "storage_bucket" {
+    source = "git::https://github.com/codewithnayak/gcp-generic-tf-templates.git//modules/k8s-sa?ref=main"
+    cluster_name = "iels-gke-oct-jan"
+    location = "europe-west1-b"
+    annotate_service_account = false
+    gcp_service_account_email = ""
+    kubernetes_service_account_name = "terraform-iam-service-account"
+    kubernetes_service_account_namespace = "jenkins"
 }
