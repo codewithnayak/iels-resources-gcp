c2a9b14a27724dc0979092ae8184e1c3a9d72c02
codewithnayak <codewithnayak@gmail.com>
2024-10-19 14:13:58 +0100

    feat: added gcp annotation

--- k8-sa/main.tf
+++ k8-sa/main.tf
@@ -1,9 +1,9 @@
 module "kubernetes_sa" {
- source = "git::https://github.com/codewithnayak/gcp-generic-tf-templates.git//modules/k8s-sa?ref=main"
- cluster_name = var.cluster_name
- location = var.location
- annotate_service_account = var.annotate_service_account
- gcp_service_account_email = var.gcp_service_account_email
- kubernetes_service_account_name = var.kubernetes_service_account_name
- kubernetes_service_account_namespace = var.kubernetes_service_account_namespace
+    source = "git::https://github.com/codewithnayak/gcp-generic-tf-templates.git//modules/k8s-sa?ref=main"
+    cluster_name = var.cluster_name
+    location = var.location
+    annotate_service_account = var.annotate_service_account
+    gcp_service_account_email = var.gcp_service_account_email
+    kubernetes_service_account_name = var.kubernetes_service_account_name
+    kubernetes_service_account_namespace = var.kubernetes_service_account_namespace
 }
--- k8-sa/terraform.tfvars
+++ k8-sa/terraform.tfvars
@@ -1,6 +1,6 @@
 cluster_name = "iels-gke-oct-jan"
 location = "europe-west1-b"
-annotate_service_account = false
-gcp_service_account_email = ""
+annotate_service_account = true
+gcp_service_account_email = "terraform-iam-svc-account@keen-ally-438611-t7.iam.gserviceaccount.com"
 kubernetes_service_account_name = "terraform-iam-service-account"
 kubernetes_service_account_namespace = "jenkins"
